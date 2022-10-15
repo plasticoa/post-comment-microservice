3bc65e53cc757e5e60ffb3fa97f5889b1b8f77ff
Allan Alonzo <2988752920101@ingenieria.usac.edu.gt>
2022-10-15 13:41:54 -0600

    Event bus initial implementation

---
+++ events/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+//incoming events
+
+app.post('/events', (req, res) => {
+    //get the event 
+    const event = req.body;
+    //run the other needed request
+    axios.post('http://localhost:4000/events', event);
+    axios.post('http://localhost:4001/events', event);
+    axios.post('http://localhost:4002/events', event);
+
+    res.send({ status: 'OK' });
+})
+
+app.listen(4005, () => {
+    console.log("Listening on 4005")
+});
