7062b17bc661a7b2fda7e52e52c4fadc3f71aa55
Allan Alonzo <2988752920101@ingenieria.usac.edu.gt>
2022-10-15 19:35:43 -0600

    service query created

--- events/index.js
+++ events/index.js
@@ -11,9 +11,15 @@ app.post('/events', (req, res) => {
     //get the event 
     const event = req.body;
     //run the other needed request
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
+    axios.post('http://localhost:4000/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4001/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4002/events', event).catch((err) => {
+        console.log(err.message);
+    });
 
     res.send({ status: 'OK' });
 })
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 // for parsing application/json
@@ -17,8 +18,9 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
+
 //create a post
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     //generate a random id for the post
     const id = randomBytes(4).toString('hex');
     //get the title and content from the request body
@@ -28,9 +30,21 @@ app.post('/posts', (req, res) => {
         id,
         tittle
     };
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, tittle
+        }
+    })
     //send the response 
     res.status(201).send(posts[id]);
 });
+//recive any event incoming from event bus
+app.post('/events', (req, res) => {
+    console.log("received event", req.body.type);
+    res.send({});
+})
 //app.listen to start a Node.js server instance
 app.listen(4000, () => {
     console.log('Listening on 4000');
