6c33b1323ce95cd6700f429be2e998c80338deef
Allan Alonzo <2988752920101@ingenieria.usac.edu.gt>
2022-10-16 00:06:38 -0600

    Moderation UI comments message updated

--- src/CommentList.js
+++ src/CommentList.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const CommentList = ({ comments }) => {
     const renderComment = comments.map((comment) => {
-        return <li key={comment.id}>{comment.content}</li>
+        let content;
+        if (comment.status === 'approved') {
+            content = comment.content;
+        }
+        if (comment.status === 'rejected') {
+            content = 'This comment has been rejected';
+        }
+        if (comment.status === 'pending') {
+            content = 'This comment is awaiting moderation';
+        }
+        return <li key={comment.id}>{content}</li>
     });
 
     return (
